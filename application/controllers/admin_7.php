9d3f8c0548cb51e1492b7bb8fc6ca5adc13943f5
thecrown <amienkurniawan5@gmail.com>
2017-07-14 11:16:20 +0700

    14 juli 2017

---
+++ application/controllers/admin_7.php
@@ -0,0 +1,13 @@
+<?php 
+class Admin_5 extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        if($this->session->curent_bidang_user != 5){
+            redirect ('login');
+        }
+    }
+    public function index(){
+        echo "berhasil login psdm";
+        echo $this->session->curent_name_user;
+    }
+}
