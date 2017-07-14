34997e6c574f443f641f6ea7b39f875bdc7f1d75
thecrown <amienkurniawan5@gmail.com>
2017-07-14 11:19:53 +0700

    14 juli 2017

--- application/controllers/admin_6.php
+++ application/controllers/admin_6.php
@@ -2,7 +2,7 @@
 class Admin_5 extends CI_Controller{
     function __construct(){
         parent::__construct();
-        if($this->session->curent_bidang_user != 5){
+        if($this->session->curent_bidang_user != 6){
             redirect ('login');
         }
     }
